b8626482722f2b124d556fdcd94c1b4f65f3dbec
Iqbal Anwar <iqbalaanwar@gmail.com>
2020-01-02 12:57:57 -0500

    Intro to data types in python

---
+++ data_types.py
@@ -0,0 +1,9 @@
+# The different data types in python
+
+# python uses 'ducktyping', "if it walks like a duck it's that"
+# Look at the type of the following:
+# x = 7
+# x = 7.0
+# x = 'hi'
+print('x is {}'.format(x))
+print(type(x))
