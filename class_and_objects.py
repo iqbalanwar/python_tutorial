4e3cfc654e58ba7e18df7925aeeee4aa292d5c8c
Iqbal Anwar <iqbalaanwar@gmail.com>
2020-01-02 08:38:58 -0500

    Basics of classes and objects

--- class_and_objects.py
+++ class_and_objects.py
@@ -1,9 +1,21 @@
+# Classes and Objects Lesson
+
 class Duck:
+    # class variables are called properties
+    sound = 'Quaaack'
+    walking = 'Walks like a duck'
+
+    # functions are called methods
+    # first argument in methods is always self
+    # self is python's version of 'this'
+    # it references the object itself
     def quack(self):
-        print('Quaaack!')
+        # print('Quaaack!')
+        print(self.sound)
     
     def walk(self):
-        print('Walks like a duck.')
+        # print('Walks like a duck.')
+        print(self.walking)
 
 def main():
     donald = Duck()
