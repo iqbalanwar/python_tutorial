827cc86020ae4eda3cbe2415fbcadf9bbcc0a8de
Iqbal Anwar <iqbalaanwar@gmail.com>
2020-01-03 18:04:05 -0500

    Continuing data types lesson

--- data_types.py
+++ data_types.py
@@ -1,9 +1,13 @@
-# The different data types in python
-
-# python uses 'ducktyping', "if it walks like a duck it's that"
-# Look at the type of the following:
-# x = 7
+''' 
+The different data types in python
+python uses 'ducktyping', "if it walks like a duck it's that"
+Look at the type of the following: 
+'''
+x = 7 / 3
 # x = 7.0
-# x = 'hi'
+# x = "seven, {}, {}".format(8, 9) # single and double quotes are the same
+# a = 8
+# b = 9
+# x = f'seven {a} {b}'
 print('x is {}'.format(x))
 print(type(x))
