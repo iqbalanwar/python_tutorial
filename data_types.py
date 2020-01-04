e2be5c153f0e8a31226e8a51ec4169c3db0f79dc
Iqbal Anwar <iqbalaanwar@gmail.com>
2020-01-03 19:41:10 -0500

    Numerial data types learned

--- data_types.py
+++ data_types.py
@@ -3,7 +3,11 @@ The different data types in python
 python uses 'ducktyping', "if it walks like a duck it's that"
 Look at the type of the following: 
 '''
+
+from decimal import *
+
 x = 7 / 3
+# x = 7 // 3 <- returns a whole
 # x = 7.0
 # x = "seven, {}, {}".format(8, 9) # single and double quotes are the same
 # a = 8
@@ -11,3 +15,10 @@ x = 7 / 3
 # x = f'seven {a} {b}'
 print('x is {}'.format(x))
 print(type(x))
+
+# y = .1 + .1 + .1 - .3
+a = Decimal('.10')
+b = Decimal('.30')
+y = a + a + a - b
+print('y is {}'.format(y))
+print(type(y))
--- standardplot.py
+++ standardplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-import this
+# import this <--- this gives The Zen of Python
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
