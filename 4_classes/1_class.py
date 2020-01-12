8a7e4e08c79d78a7667681ca56e0c31ffdc6ade5
Iqbal Anwar <iqbalaanwar@gmail.com>
2020-01-12 18:02:33 -0500

    Created Animal class with getters

---
+++ 4_classes/1_class.py
@@ -0,0 +1,51 @@
+'''
+When an object is created from a class, 'self' will reference
+that object. Everything that is defined in the class is
+dereferenced off itself to get the instantiated object version
+of it (through the period '.' operator).
+- self is a reference to the object (like 'this' in JS/Java)
+- self is always used as the first parameter
+
+See the use of this in main(). Remember, quack() and move() are
+objects.
+
+Putting an underscore at the beginning of the variables discourages
+users of the object to access the variables directly. That's why the
+getters are there.
+'''
+
+class Animal:
+    # standard constructor:
+    # def __init__(self, type, name, sound):
+    #     self._type = type
+    #     self._name = name
+    #     self._sound = sound
+
+    # constructor with multiple keyword arguments
+    def __init__(self, **kwargs):
+        self._type = kwargs['type'] if 'type' in kwargs else 'kitten'
+        self._name = kwargs['name'] if 'name' in kwargs else 'fluffy'
+        self._sound = kwargs['sound'] if 'sound' in kwargs else 'rawr'
+    
+    def type(self): return self._type
+    def name(self): return self._name
+    def sound(self): return self._sound
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print('The {} is named "{}" and says "{}".'.format(o.type(), o.name(), o.sound()))
+
+
+def main():
+
+    a0 = Animal(type = 'kitten', name = 'fluffy', sound = 'rawr')
+    a1 = Animal(type = 'puppy', name = 'pal', sound = 'woof')
+    # object instantiation with standard constructor:
+    # a1 = Animal('puppy', 'pal', 'woof')
+    print_animal(a0)
+    print_animal(a1)
+    print_animal(Animal(type = 'velociraptor', name = 'veronica', sound = 'hello'))
+    print_animal(Animal())
+
+if __name__ == '__main__': main()
